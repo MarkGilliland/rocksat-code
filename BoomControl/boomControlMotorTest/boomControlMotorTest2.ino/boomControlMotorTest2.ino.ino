1bfcb930663ff7c904d65d68851a4c0d176fe6b9
Brad <nooktheraccoon@gmail.com>
2021-04-02 11:32:20 -0600

    push before pull, minor boom test code edits

--- BoomControl/boomControlMotorTest/boomControlMotorTest2.ino/boomControlMotorTest2.ino.ino
+++ BoomControl/boomControlMotorTest/boomControlMotorTest2.ino/boomControlMotorTest2.ino.ino
@@ -11,10 +11,10 @@
 #define M2CW A3
 #define FRONT_SENSOR 4
 #define BACK_SENSOR 5
-#define DIRECTION 1 // 1 signifies out, 0 signifies in, on camera boom.
-#define COMMS_EXTENSION_TIME 9000
+#define DIRECTION 0 // 1 signifies out, 0 signifies in, on camera boom.
+#define COMMS_EXTENSION_TIME 10000
 #define CAMERA_EXTENSION_TIME 36000
-#define TIMER COMMS_EXTENSION_TIME // 1.25" per sec, ~36sec for full extension on camera boom.
+#define TIMER 300//CAMERA_EXTENSION_TIME // 1.25" per sec, ~36sec for full extension on camera boom.
 //Attach encoder interrupts to pins 2 and 9
 //Encoder myEnc(2, 9);
 
@@ -23,7 +23,7 @@ bool commsStuck = false;
 void setup() {
   //Start serial connection
   Serial.begin(9600);
-  
+
   //Set motor control pins as outputs
   pinMode(ENABLE, OUTPUT);
   pinMode(M1CCW, OUTPUT);
@@ -33,110 +33,110 @@ void setup() {
   pinMode(FRONT_SENSOR, INPUT);
   pinMode(BACK_SENSOR, INPUT);
 
-//  //TESTING THE ***COMMS*** BOOM!!!
-//  digitalWrite(ENABLE, HIGH);
-//  if(DIRECTION){
-//    digitalWrite(M2CW, LOW);
-//    digitalWrite(M2CCW, HIGH);
-//  }else{
-//    digitalWrite(M2CW, HIGH);
-//    digitalWrite(M2CCW, LOW);
-//  }
-//  digitalWrite(13,HIGH);
-//  digitalWrite(M1CW, LOW);
-//  digitalWrite(M1CCW, LOW);
-//  delay(TIMER);
-//  digitalWrite(M2CW, LOW);
-//  digitalWrite(M2CCW, LOW);
-//  digitalWrite(13,LOW);
+    //TESTING THE CAMERA BOOM
+    digitalWrite(ENABLE, HIGH);
+    if(DIRECTION){
+      digitalWrite(M1CW, LOW);
+      digitalWrite(M1CCW, HIGH);
+    }else{
+      digitalWrite(M1CW, HIGH);
+      digitalWrite(M1CCW, LOW);
+    }
+    digitalWrite(13,HIGH);
+    digitalWrite(M2CW, LOW);
+    digitalWrite(M2CCW, LOW);
+    delay(TIMER);
+    digitalWrite(M1CW, LOW);
+    digitalWrite(M1CCW, LOW);
+    digitalWrite(13,LOW);
 
-    // CYCLE TESTING COMMS BOOM
-//  bool deploying = true;
-//  int numCycles = 10;
-//  digitalWrite(13,HIGH);
+  // CYCLE TESTING COMMS BOOM
+  //  bool deploying = true;
+  //  int numCycles = 10;
+  //  digitalWrite(13,HIGH);
+  //  digitalWrite(M1CW, LOW);
+  //  digitalWrite(M1CCW, LOW);
+  //  if(digitalRead(FRONT_SENSOR)){
+  //    digitalWrite(M2CW, HIGH);
+  //    digitalWrite(M2CCW, LOW);
+  //    deploying = false;
+  //  }else{
+  //    digitalWrite(M2CW, LOW);
+  //    digitalWrite(M2CCW, HIGH);
+  //  }
+  //  for(int i = 0; i < numCycles; i++){
+  //    while(deploying){
+  //      digitalWrite(ENABLE,HIGH);
+  //      if(digitalRead(FRONT_SENSOR)){
+  //        digitalWrite(ENABLE,LOW);
+  //        digitalWrite(M2CW, HIGH);
+  //        digitalWrite(M2CCW, LOW);
+  //        deploying = false;
+  //        break;
+  //      }
+  //    }
+  //    while(!deploying){
+  //      digitalWrite(ENABLE,HIGH);
+  //      if(digitalRead(BACK_SENSOR)){
+  //        digitalWrite(ENABLE,LOW);
+  //        digitalWrite(M2CW, LOW);
+  //        digitalWrite(M2CCW, HIGH);
+  //        deploying = true;
+  //        break;
+  //      }
+  //    }
+  //  }
+
+  //  EXTEND/RETRACT COMMS BOOM
+//  bool deploying = 0;
+//  digitalWrite(13, HIGH);
 //  digitalWrite(M1CW, LOW);
 //  digitalWrite(M1CCW, LOW);
-//  if(digitalRead(FRONT_SENSOR)){
-//    digitalWrite(M2CW, HIGH);
-//    digitalWrite(M2CCW, LOW);
-//    deploying = false;
-//  }else{
-//    digitalWrite(M2CW, LOW);
-//    digitalWrite(M2CCW, HIGH);
-//  }
-//  for(int i = 0; i < numCycles; i++){
-//    while(deploying){
-//      digitalWrite(ENABLE,HIGH);
-//      if(digitalRead(FRONT_SENSOR)){
-//        digitalWrite(ENABLE,LOW);
-//        digitalWrite(M2CW, HIGH);
-//        digitalWrite(M2CCW, LOW);
-//        deploying = false;
+//  int startTime = millis();
+//  int stopTime = startTime;
+//  if (deploying) {
+//    while (!digitalRead(FRONT_SENSOR)) {
+//      digitalWrite(ENABLE, HIGH);
+//      digitalWrite(M2CW, LOW);
+//      digitalWrite(M2CCW, HIGH);
+//      stopTime = millis();
+//      if ((stopTime - startTime) > 1.5 * COMMS_EXTENSION_TIME) {
+//        commsStuck = true;
+//        digitalWrite(ENABLE, LOW);
 //        break;
 //      }
 //    }
-//    while(!deploying){
-//      digitalWrite(ENABLE,HIGH);
-//      if(digitalRead(BACK_SENSOR)){
-//        digitalWrite(ENABLE,LOW);
-//        digitalWrite(M2CW, LOW);
-//        digitalWrite(M2CCW, HIGH);
-//        deploying = true;
+//    digitalWrite(ENABLE, LOW);
+//  } else {
+//    while (!digitalRead(BACK_SENSOR)) {
+//      digitalWrite(ENABLE, HIGH);
+//      digitalWrite(M2CW, HIGH);
+//      digitalWrite(M2CCW, LOW);
+//      stopTime = millis();
+//      if ((stopTime - startTime) > 1.5 * COMMS_EXTENSION_TIME) {
+//        commsStuck = true;
+//        digitalWrite(ENABLE, LOW);
 //        break;
 //      }
 //    }
+//    digitalWrite(ENABLE, LOW);
 //  }
-
-//  EXTEND/RETRACT COMMS BOOM
-  bool deploying = 0;
-  digitalWrite(13,HIGH);
-  digitalWrite(M1CW, LOW);
-  digitalWrite(M1CCW, LOW);
-  int startTime = millis();
-  int stopTime = startTime;
-  if(deploying){
-    while(!digitalRead(FRONT_SENSOR)){
-      digitalWrite(ENABLE,HIGH);
-      digitalWrite(M2CW, LOW);
-      digitalWrite(M2CCW, HIGH);
-      stopTime = millis();
-      if((stopTime - startTime) > 1.5*COMMS_EXTENSION_TIME){
-        commsStuck = true;
-        digitalWrite(ENABLE,LOW);
-        break;
-      }
-    }
-    digitalWrite(ENABLE,LOW);
-  }else{
-    while(!digitalRead(BACK_SENSOR)){
-      digitalWrite(ENABLE,HIGH);
-      digitalWrite(M2CW, HIGH);
-      digitalWrite(M2CCW, LOW);
-      stopTime = millis();
-      if((stopTime - startTime) > 1.5*COMMS_EXTENSION_TIME){
-        commsStuck = true;
-        digitalWrite(ENABLE,LOW);
-        break;
-      }
-    }
-    digitalWrite(ENABLE,LOW);
-  }
 }
 
 void loop() {
   int blinkTime = 250;
-  if(commsStuck){
+  if (commsStuck) {
     blinkTime = 750;
     Serial.println("Comms boom stuck.");
   }
   //Serial.println(myEnc.read());
-  digitalWrite(13,HIGH);
+  digitalWrite(13, HIGH);
   delay(blinkTime);
-  digitalWrite(13,LOW);
+  digitalWrite(13, LOW);
   delay(blinkTime);
   Serial.print("Front: ");
   Serial.println(digitalRead(FRONT_SENSOR));
   Serial.print("Back: ");
   Serial.println(digitalRead(BACK_SENSOR));
-  
+
 }
